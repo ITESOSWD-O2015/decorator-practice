7b9b685d25cd8831d6d568d1022ae4970eece7b8
Guillermo <uriakua@gmail.com>
2015-10-06 08:50:33 -0500

    minor style changes

--- src/main/java/com/iteso/decorator/size/SizeDecorator.java
+++ src/main/java/com/iteso/decorator/size/SizeDecorator.java
@@ -1,17 +1,8 @@
 package com.iteso.decorator;
 
 /**
- * Created with IntelliJ IDEA.
- * User: rvillalobos
- * Date: 8/19/13
- * Time: 12:46 PM
- * To change this template use File | Settings | File Templates.
+ * Created by Guillermo Roldan
  */
-/*
-public abstract class CondimentsDecorator extends Taco {
-    public abstract String getDescription();
-}
-*/
 
 public abstract class SizeDecorator extends Taco{
     public abstract String getDescription();
